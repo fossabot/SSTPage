dedbee494ca11f373a9833b37a43a298e8689f6a
Losses Don <losses.don@gmail.com>
2017-08-11 13:54:09 +0800

    Hide reduent texts in member card.

--- source/server/modules/pageRendering/template.js
+++ source/server/modules/pageRendering/template.js
@@ -4,6 +4,9 @@ const template =  ({ body, helmet, pageData }) => {
     <html ${helmet.htmlAttributes.toString()}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="theme-color" content="#9c27b0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <link rel="stylesheet" href="/assets/bundle.css">
         ${helmet.title.toString()}
         ${helmet.meta.toString()}
