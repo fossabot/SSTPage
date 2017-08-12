19b433272dd593d998a3ce3f093f595674289f1a
Losses Don <losses.don@gmail.com>
2017-08-12 10:00:58 +0800

    Adapting member list page to small screen.

--- source/client/components/Member/MemberListUnit.jsx
+++ source/client/components/Member/MemberListUnit.jsx
@@ -8,28 +8,32 @@ import Button from 'material-ui/Button'
 import EmailIcon from 'material-ui-icons/Email'
 import HomeIcon from 'material-ui-icons/Home'
 
+import './stylesheets/MemberListUnit.less'
+
 class MemberListUnit extends React.Component{
   render(){
     return (
-      <Grid item md={4} xs={12} className="member">
-        <div className="member_photo">
-          <img src={`/assets/user/images/face/${this.props.faceImage}`} alt={`A photo of ${this.props.name}`} />
+      <Grid item md={4} className="member_list_unit">
+        <div className="member_list_unit_container">
+          <div className="member_photo">
+            <img src={`/assets/user/images/face/${this.props.faceImage}`} alt={`A photo of ${this.props.name}`} />
+          </div>
+          <div className="information">
+            <p className="name_line">
+              <span className="name">{this.props.name}</span>
+              <span className="title">{this.props.title}</span>
+            </p>
+            <p className="study_direction">{this.props.researchDirection}</p>
+          </div>
+          <Grid container spacing={0} className="contact_buttons">
+            <Grid item xs={6}>
+              <Button><EmailIcon />邮件联系</Button>
+            </Grid>
+            <Grid item sm={6}>
+              <Button><HomeIcon />学术首页</Button>
+            </Grid>
+          </Grid>
         </div>
-        <div className="information">
-        <p className="name_line">
-          <span className="name">{this.props.name}</span>
-          <span className="title">{this.props.title}</span>
-        </p>
-        <p className="study_direction">{this.props.researchDirection}</p>
-        <Grid container className="contact_buttons">
-          <Grid item xs={6}>
-            <Button><EmailIcon />邮件联系</Button>
-          </Grid>
-          <Grid item sm={6}>
-            <Button><HomeIcon />学术首页</Button>
-          </Grid>
-        </Grid>
-      </div>
       </Grid>
     )
   }
