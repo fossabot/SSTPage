ac1ee992c3411f9da05a8cdb449bcf1997c17a2d
Losses Don <losses.don@gmail.com>
2017-08-27 19:07:32 +0800

    Add favicons.

--- source/server/modules/pageRendering/template.js
+++ source/server/modules/pageRendering/template.js
@@ -5,8 +5,17 @@ const template =  ({ body, helmet, pageData, siteConfiguration,  jssCss }) => {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <meta name="theme-color" content="#9c27b0">
+        <meta name="application-name" content="SSTPage">
+        <meta name="msapplication-config" content="/assets/user/favicon/browserconfig.xml">
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="SSTPage">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/user/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/user/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/user/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/assets/user/favicon/manifest.json">
+        <link rel="mask-icon" href="/assets/user/favicon/safari-pinned-tab.svg" color="#212121">
+        <link rel="shortcut icon" href="/assets/user/favicon/favicon.ico">
         <link rel="stylesheet" href="/assets/bundle.css">
         ${helmet.title.toString()}
         ${helmet.meta.toString()}
