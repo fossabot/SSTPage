0f165b4a9b4b9a3614eca78e24070c765e2f61c0
Losses Don <losses.don@gmail.com>
2018-01-20 17:18:42 +0800

    Bug fix of tutor introduction page.

--- source/client/components/Member/TutorIntroduction.jsx
+++ source/client/components/Member/TutorIntroduction.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 import Face from '../Face/Face'
 
+import './stylesheets/TutorIntroduction.less'
+
 class TutorIntroduction extends React.Component{
   render(){
     return (
       <div className="tutor_introduction_content flexbox">
         <Face src="LCM.jpg" size="large" />
         <div className="main_text flexbox">
-          {this.props.children}        
+          {this.props.children}
         </div>
       </div>
     )
